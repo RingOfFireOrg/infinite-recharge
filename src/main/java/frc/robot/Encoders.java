6764885fd63c762fffea0624e131d6045abb5332
jasonw52603 <minecrafter10k@gmail.com>
2020-01-14 18:55:54 -0500

    Added NEO code from 2019

---
+++ src/main/java/frc/robot/Encoders.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+public class Encoders {
+    private Encoder leftEncoder = new Encoder(RobotMap.DRIVE_TRAIN_LEFT_ENCODER_A,
+                                              RobotMap.DRIVE_TRAIN_LEFT_ENCODER_B, false, Encoder.EncodingType.k1X);
+    private Encoder rightEncoder = new Encoder(RobotMap.DRIVE_TRAIN_RIGHT_ENCODER_A, 
+                                               RobotMap.DRIVE_TRAIN_RIGHT_ENCODER_B, false, Encoder.EncodingType.k1X);
+
+                                               
+    public double getEncoderVal(Encoder encoder){
+        return encoder.getDistance();
+    }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,14 +7,42 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 public class Robot extends TimedRobot {
+
+  Joystick rightstick = new Joystick(0);
+  Joystick leftstick = new Joystick(1);
+  Joystick manipulatorStick = new Joystick(2);
+
+  AHRS ahrs;
+
+  NeoTankDrive neoDrive;
+  
+  public CANSparkMax frontLeftMotor;
+  public CANSparkMax frontRightMotor;
+  public CANSparkMax backRightMotor;
+  public CANSparkMax backLeftMotor;
+
   @Override
   public void robotInit() {
+    ahrs = new AHRS(SerialPort.Port.kUSB);
+    ahrs.reset();
+
+    neoDrive = new NeoTankDrive();
+    
+    frontLeftMotor = new CANSparkMax(RobotMap.NEO_FRONT_LEFT, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(RobotMap.NEO_FRONT_RIGHT, MotorType.kBrushless);
+    backRightMotor = new CANSparkMax(RobotMap.NEO_BACK_RIGHT, MotorType.kBrushless);
+    backLeftMotor = new CANSparkMax(RobotMap.NEO_BACK_LEFT, MotorType.kBrushless);
   }
 
   @Override
@@ -23,14 +51,20 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
+
   }
 
   @Override
   public void autonomousPeriodic() {
+
   }
 
   @Override
   public void teleopPeriodic() {
+    double rightSpeed = rightstick.getY();
+    double leftSpeed = leftstick.getY();
+
+    neoDrive.drive(rightSpeed, leftSpeed, 1.0, true);
   }
 
   @Override
