634282c61af77b63505638c016988b20f40b2038
jasonw52603 <minecrafter10k@gmail.com>
2020-01-21 16:07:31 -0500

    added manipulatorStick joystick

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,65 +19,65 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot extends TimedRobot {
 
-  Joystick rightstick = new Joystick(0);
-  Joystick leftstick = new Joystick(1);
-  Joystick manipulatorStick = new Joystick(2);
+    Joystick rightstick = new Joystick(0);
+    Joystick leftstick = new Joystick(1);
+    Joystick manipulatorStick = new Joystick(2);
 
-  AHRS ahrs;
+    AHRS ahrs;
 
-  NeoTankDrive neoDrive;
+    NeoTankDrive neoDrive;
 
-  Vision vision;
+    Vision vision;
 
-  double tx;
-  double ty;
-  double ta;
-  
-  public CANSparkMax frontLeftMotor;
-  public CANSparkMax frontRightMotor;
-  public CANSparkMax backRightMotor;
-  public CANSparkMax backLeftMotor;
+    double tx;
+    double ty;
+    double ta;
 
-  @Override
-  public void robotInit() {
-    ahrs = new AHRS(SerialPort.Port.kUSB);
-    ahrs.reset();
+    public CANSparkMax frontLeftMotor;
+    public CANSparkMax frontRightMotor;
+    public CANSparkMax backRightMotor;
+    public CANSparkMax backLeftMotor;
 
-    neoDrive = new NeoTankDrive();
-    
-    frontLeftMotor = new CANSparkMax(RobotMap.NEO_FRONT_LEFT, MotorType.kBrushless);
-    frontRightMotor = new CANSparkMax(RobotMap.NEO_FRONT_RIGHT, MotorType.kBrushless);
-    backRightMotor = new CANSparkMax(RobotMap.NEO_BACK_RIGHT, MotorType.kBrushless);
-    backLeftMotor = new CANSparkMax(RobotMap.NEO_BACK_LEFT, MotorType.kBrushless);
-    vision = new Vision();
-  }
+    @Override
+    public void robotInit() {
+        ahrs = new AHRS(SerialPort.Port.kUSB);
+        ahrs.reset();
 
-  @Override
-  public void robotPeriodic() {
-  }
+        neoDrive = new NeoTankDrive();
 
-  @Override
-  public void autonomousInit() {
+        frontLeftMotor = new CANSparkMax(RobotMap.NEO_FRONT_LEFT, MotorType.kBrushless);
+        frontRightMotor = new CANSparkMax(RobotMap.NEO_FRONT_RIGHT, MotorType.kBrushless);
+        backRightMotor = new CANSparkMax(RobotMap.NEO_BACK_RIGHT, MotorType.kBrushless);
+        backLeftMotor = new CANSparkMax(RobotMap.NEO_BACK_LEFT, MotorType.kBrushless);
+        vision = new Vision();
+    }
 
-  }
+    @Override
+    public void robotPeriodic() {
+    }
 
-  @Override
-  public void autonomousPeriodic() {
+    @Override
+    public void autonomousInit() {
 
-  }
+    }
 
-  @Override
-  public void teleopPeriodic() {
-    double rightSpeed = rightstick.getY();
-    double leftSpeed = leftstick.getY();
+    @Override
+    public void autonomousPeriodic() {
 
-    neoDrive.drive(rightSpeed, leftSpeed, 1.0, true);
+    }
 
-    vision.updateVisionVals(); 
-    vision.getTargetDistance();
-  }
+    @Override
+    public void teleopPeriodic() {
+        double rightSpeed = rightstick.getY();
+        double leftSpeed = leftstick.getY();
 
-  @Override
-  public void testPeriodic() {
-  }
+        neoDrive.drive(rightSpeed, leftSpeed, 1.0, true);
+
+        vision.updateVisionVals();
+        vision.getTargetDistance();
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
 }
