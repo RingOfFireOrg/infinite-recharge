0ab9f88fdeb5e81cf875f86d0e328615c93bae48
Duncan Cameron <duncanbuggo@gmail.com>
2020-02-20 15:06:09 -0500

    stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,6 +18,9 @@ import edu.wpi.first.wpilibj.smartdashboard.*;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
 
 
 public class Robot extends TimedRobot {
@@ -98,6 +101,17 @@ public class Robot extends TimedRobot {
         SmartDashboard.putNumber("Shooter speed", shooterSpeed * speedMultiplier);
         //vision.updateVisionVals();
         //vision.getTargetDistance();
+
+
+// Creates UsbCamera and MjpegServer [1] and connects them
+CameraServer.getInstance().startAutomaticCapture();
+
+// Creates the CvSink and connects it to the UsbCamera
+CvSink cvSink = CameraServer.getInstance().getVideo();
+
+// Creates the CvSource and MjpegServer [2] and connects them
+CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+
     }
 
         
