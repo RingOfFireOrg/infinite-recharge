bc0aa3883e25c24e6fb117a9b316f92e06bca7af
jasonw52603 <minecrafter10k@gmail.com>
2020-02-24 21:02:17 -0500

    updated wpilib version

--- src/main/java/frc/robot/VisionStates.java
+++ src/main/java/frc/robot/VisionStates.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
 public enum VisionStates {
+<<<<<<< Updated upstream
         PID_RESET, PID_UPDATE, SET_MOTORS, CHECK_LINEUP
 }
+=======
+    PID_RESET, PID_UPDATE, SET_MOTORS, CHECK_LINEUP
+}
+>>>>>>> Stashed changes
