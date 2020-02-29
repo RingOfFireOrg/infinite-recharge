e033c874214c50e5971a5f1b3d6db660ebd5651e
Connor Mitchell <connorbrannonmitchell@gmail.com>
2020-02-29 17:02:24 -0500

    added smart dashboard to autonomous

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -43,6 +43,7 @@ public class Autonomous {
         autonomousChooser.setDefaultOption(DriveAndShoot, DriveAndShoot);
         autonomousChooser.addOption(StraightShot, StraightShot);
         autonomousChooser.addOption(SimpleDrive, SimpleDrive);
+        SmartDashboard.putData(autonomousChooser);
 
     }
 
