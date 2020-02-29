9d9e22027b8bf1cff7b51cd7b2d2f5d7a832d4d6
Connor Mitchell <connorbrannonmitchell@gmail.com>
2020-02-29 13:17:13 -0500

    intake sped up

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -46,9 +46,9 @@ public class Intake extends InternalSubsystem {
         if (state == IntakeStates.IDLE) {
             intakeMotor.set(0);
         } else if (state == IntakeStates.OUT) {
-            intakeMotor.set(-0.1);
+            intakeMotor.set(-0.2);
         } else if (state == IntakeStates.IN) {
-            intakeMotor.set(0.1);
+            intakeMotor.set(0.2);
         }
     }
 }
